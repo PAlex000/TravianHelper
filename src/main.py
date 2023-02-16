0388cc4a28896f600d3ede7916959e148111220e
Pozsgai <jatekalex@gmail.com>
2023-02-16 12:02:57 +0100

    Modify main.py

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
-from gui.login_gui import *
 from tkinter import messagebox
 from messages.error_messages import *
 from selenium.webdriver.support import expected_conditions as EC
-from gui.login_gui import Login
+from gui.login_gui import Login_page
+from gui.login_gui import Server_page
+
 
 class App:
 
@@ -19,12 +20,11 @@ class App:
         # A dict which has "server_name" : "its button reference" key-values
         self.__container = dict()
         # It's for checking if the selected world is first or not
-        self.__current_world = True
+        self.__first_world = True
 
     def __get_login_credentials(self):
         # login_credentials[0] is email, login_credentials[1] is password
-        login_variable = Login("Login credentials", Tk())
-        login_credentials = login_variable.login_gui()
+        login_credentials = Login_page("Login credentials").login_gui()
         if len(login_credentials) == 0:
             messagebox.showerror(LOGIN_ERROR_TITLE, LOGIN_ERROR_MSG)
             return
@@ -75,17 +75,17 @@ class App:
         self.__wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "game-world")))
 
         container_fluids = self.__driver.find_elements(By.CLASS_NAME, "game-world")
-        # TODO: Think a better way to get servers name.
+        # TODO: Think a better way to get server names.
         for element in container_fluids:
             if element\
                     .find_element(By.CSS_SELECTOR, "div.default-button") \
                     .find_element(By.TAG_NAME, "span") \
                     .get_attribute('innerHTML') == "Continue playing":
                 if len(container_fluids) > 1:
-                    if self.__current_world:
+                    if self.__first_world:
                         world_name = element.find_element(By.CLASS_NAME, "game-world-name").find_element(By.TAG_NAME, "span").get_attribute("innerHTML")
                         world_name = world_name.split("-")[1].lstrip()
-                        self.__current_world = False
+                        self.__first_world = False
                         self.__container[world_name] = element
                     else:
                         world_name = element.find_element(By.CLASS_NAME, "game-world-name").get_attribute("innerHTML")
@@ -97,7 +97,7 @@ class App:
             
     def __set_selected_world(self):
         # server_gui returns server_name and server_element (self.__selected_world[0] and [1]
-        self.__selected_world = Login.server_gui(self.__container)
+        self.__selected_world = Server_page("Server Page", self.__container).server_gui()
 
     def __login_to_the_selected_world(self):
         self.__wait.until(EC.element_to_be_clickable((self.__selected_world[1].find_element(By.CSS_SELECTOR, "div.default-button")))).click()
