219c2025878f9afb1f2b1ba0daf4507d467c613f
Pozsgai Alex <jatekalex@gmail.com>
2023-02-17 15:31:30 +0100

    Implement get_field_view function, some refactor using oop, modified building_names

--- src/webscrape/building.py
+++ src/webscrape/building.py
@@ -1,8 +1,9 @@
 class Building():
-    def __init__(self, location, name, buildingid):
+    def __init__(self, location, name, buildingid, level):
         self.__location = location
         self.__name = name
         self.__buildingid = buildingid
+        self.__level = level
         
     @property
     def location(self):
@@ -16,5 +17,26 @@ class Building():
     def buildingid(self):
         return self.__buildingid
 
-    def __str__ (self):
-        return f"Building(name = {self.name}, location = {self.location}, buildingid = {self.buildingid})"
+    @property
+    def level(self):
+        return self.__level
+    
+    def __str__(self):
+        return f"Building(name = {self.name}, location = {self.location}, buildingid = {self.buildingid}, level = {self.level})"
+
+class Infra_building(Building):
+
+    def __init__(self, location, name, buildingid, level):
+        super().__init__(location, name, buildingid, level)
+
+    def __str__(self):
+        return f"Infra_building(name = {self.name}, location = {self.location}, buildingid = {self.buildingid}, level = {self.level})"
+    
+class Field_building(Building):
+
+    def __init__(self, location, name, buildingid, level):
+        super().__init__(location, name, buildingid, level)
+
+    def __str__(self):
+        return f"Field_building(name = {self.name}, location = {self.location}, buildingid = {self.buildingid}, level = {self.level})"
+    
--- src/webscrape/infra_view.py
+++ src/webscrape/infra_view.py
@@ -1,55 +1,22 @@
-from src.webscrape.building import Building
+from src.webscrape.general_view import General_view
+from src.webscrape.building import Infra_building
 from src.webscrape.building_names import building_names
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
-class Infra_view():
+class Infra_view(General_view):
     def __init__(self, driver):
-        self.__fields = []
-        self.__driver = driver
-        self.__wait = WebDriverWait(self.__driver, 10)
-        self.__building_id = ""
-        self.__location = ""
-        self.__building_name = ""
+        super().__init__(driver)
 
-    @property
-    def fields(self):
-        return self.__fields
-
-    def __get_building_id(self, locationId):
-        self.__wait.until(EC.visibility_of_element_located((By.ID, "villageView")))
-        self.__element = self.__driver.find_element(By.CLASS_NAME, "buildingLocation" + str(locationId)).get_attribute('innerHTML')
-        start_index = self.__element.find("buildingId")
-        if start_index == -1:
-            self.__building_id = "free_slot"
-        else:
-            self.__building_id = "buildingId" + self.__element[start_index + 10] + self.__element[start_index + 11]
-        
-    def __get_building_location(self):
-        start_index = self.__element.find("location_")
-        if start_index == -1:
-            self.__location = "free_slot"
-        else:
-            self.__location = "location" + self.__element[start_index + 9] + self.__element[start_index + 10]
-
-    def __get_building_name(self):
-        if self.__building_id == "free_slot":
-            self.__building_name = "free_slot"
-        else:
-            self.__building_name = building_names[self.__building_id]
-
-    def __get_main_view(self, id):
-        self.__get_building_id(id)
-        self.__get_building_location()
-        self.__get_building_name()
-        
-    def __field_setter(self, buildingid, location, name):
-        self.__fields.append(Building(location, name, buildingid))
-
-    # Infra starts from 18 until 40
+    # Infra starts from 19 until 40
     def get_all_buildings(self):
-        for building_location in range(18,41):
-            self.__get_main_view(building_location)
-            self.__field_setter(self.__building_id, self.__location , self.__building_name)
+        self._get_to_specific_view("navi_village")
+        for building_location in range(19,41):
+            self._get_main_view(building_location, "villageView")
+            self._field_setter(self._building_id, self._location , self._building_name, Infra_building, level = self._building_level)
 
+    def __str__(self):
+        temp = ""
+        for i in self._fields:
+            temp += str(i) + "\n"
+        return temp
