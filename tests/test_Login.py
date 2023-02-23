dcd3d34bb871a1e190bee5028ebd2c95a48385a0
Pozsgai Alex <jatekalex@gmail.com>
2023-02-23 20:16:07 +0100

    Use pytest instead of unittest

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -1,41 +1,44 @@
-import unittest
+import pytest
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from src.webscrape.login import Login
 import json
 
-class TestLogin(unittest.TestCase):
+class Test_Login():
+    
+    def testLoginWithCorrectCredentials(self):
+        self.driverSetUp()
+        self.getCredentials()
+        saveEmail = 0
+        self.login = Login(self.__email, self.__password, saveEmail, self.__driver)
+        self.login.login()
+        assert self.__driver.current_url == "https://lobby.kingdoms.com/#/"
+        self.driverQuit()
 
-    def setUp(self):
-        self.__readCredentialsFromJSON()
+    def driverSetUp(self):
         self.__options = Options()
         self.__options.add_experimental_option('detach', True)
         self.__options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.__driver = webdriver.Edge(options=self.__options)
         
-    def __readCredentialsFromJSON(self):
+    def getCredentials(self):
+        self.readCredentialsFromJSON()
+
+    def readCredentialsFromJSON(self):
         with open('./credentials.json', 'r') as f:
             data = json.load(f)
             self.__email = data["email"]
             self.__password = data["password"]
-    
-    def testLoginWithCorrectCredentials(self):
-        saveEmail = 0
-        self.login = Login(self.__email, self.__password, saveEmail, self.__driver)
-        self.login.login()
-        self.assertEqual(self.__driver.current_url, "https://lobby.kingdoms.com/#/")
+            
+    def driverQuit(self):
+        self.__driver.quit()
 
     def testLoginWithIncorrectCredentials(self):
+        self.driverSetUp()
         email = "definitelyAgoodEmail@gmail.com"
         password = "1234"
         saveEmail = 0
         self.login = Login(email, password, saveEmail, self.__driver)
-        with self.assertRaises(SystemExit) as cm:
+        with pytest.raises(SystemExit):
             self.login.login()
-            self.assertEqual(cm.exception.code, 1)
-    
-    def tearDown(self):
-        self.__driver.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+        self.driverQuit()
