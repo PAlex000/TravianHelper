63c807b69cf7dca8206f18c818d4b2877c427406
Pozsgai Alex <jatekalex@gmail.com>
2023-07-14 14:20:24 +0200

    Update tests

--- tests/test_login_input_check.py
+++ tests/test_login_input_check.py
@@ -9,20 +9,13 @@ from tests import BaseTest
 class TestLoginInputCheck(BaseTest):
     def test_login_input_check_with_correct_credentials(self):
         self._set_credentials_from_json()
-        attempt = LoginInputCheck(self._email, self._password, 0)
-        attempt.get_login_credentials()
-        assert (
-            attempt.email == self._email
-            and attempt.password == self._password
-            and attempt.save_email_btn == 0
-        )
+        attempt = LoginInputCheck(self._email, self._password, 0).check_details()
+        assert attempt == True
 
     def test_login_input_check_with_incorrect_email(self):
         with pytest.raises(EmailError):
-            attempt = LoginInputCheck("ThisIsNotAnEmail", "", 0)
-            attempt.get_login_credentials()
+            LoginInputCheck("ThisIsNotAnEmail", "", 0).check_details()
 
     def test_login_input_check_with_incorrect_password(self):
         with pytest.raises(PasswordError):
-            attempt = LoginInputCheck("correctEmail@gmail.com", "", 0)
-            attempt.get_login_credentials()
+            LoginInputCheck("correctEmail@gmail.com", "", 0).check_details()
